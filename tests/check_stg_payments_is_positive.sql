2561a6e7656eb972f8d6e1b920c5f0491426ceef
Sharath Sasidaran <Sharath957@gmail.com>
2025-05-07 03:48:16 +0000

    test for stg payments to check all amount for each order id is positive.

---
+++ tests/check_stg_payments_is_positive.sql
@@ -0,0 +1,13 @@
+WITH payment AS (
+    select * from {{ ref('stg_stripe__payments') }}
+)
+
+select 
+    order_id,
+    sum(amount) as total_amount
+from
+    payment
+group by 
+    order_id
+having  
+    total_amount < 0
